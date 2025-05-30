c89dbfa8f35f199f17065f9f1ddc82a94cca17f4
Rodrigo Fernández Urbina <92340593+AraAraMusha@users.noreply.github.com>
2025-05-30 10:57:09 -0600

    Update Lab15ShadedCube.js

--- Lab15/Lab15ShadedCube.js
+++ Lab15/Lab15ShadedCube.js
@@ -83,12 +83,12 @@ var materialShininess = 51.2;
 */
 
 // pewter
-/*
+/
 var materialAmbient  = vec4(0.10588 , 0.058824 , 0.113725 , 1.0);
 var materialDiffuse  = vec4(0.42745 , 0.470588 , 0.541176 , 1.0);
 var materialSpecular  = vec4(0.3333 , 0.3333 , 0.521569 , 1.0);
 var materialShininess = 9.84615;
-*/
+/
 
 // silver
 /*
@@ -99,12 +99,12 @@ var materialShininess = 51.2;
 */
 
 // polished silver
-//
+/*
 var materialAmbient  = vec4(0.23125 , 0.23125 , 0.23125 , 1.0);
 var materialDiffuse  = vec4(0.2775 , 0.2775 , 0.2775 , 1.0);
 var materialSpecular  = vec4(0.773911 , 0.773911 , 0.773911 , 1.0);
 var materialShininess = 89.6;
-//
+*/
 
 var ctm;
 var ambientColor, diffuseColor, specularColor;
